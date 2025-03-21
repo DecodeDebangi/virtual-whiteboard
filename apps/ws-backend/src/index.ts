09829684dec102e81e7e8a56065644fd111295d6
Debangi <debangiofficial@gmail.com>
2025-03-22 03:32:04 +0530

    added token validation to websocket server

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,10 +1,29 @@
 import { WebSocketServer } from "ws";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws) => {
-  ws.on("message", (message) => {
-    console.log(`Received message => ${message}`);
+wss.on("connection", (ws, request) => {
+  const url = request.url;
+  if (!url || !url.includes("?") || !url.startsWith("ws://")) {
+    return;
+  }
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") || "";
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET!) as jwt.JwtPayload;
+  if (!decoded || !decoded.userId) {
+    ws.close();
+    return;
+  }
+
+  const userId = decoded.userId;
+  console.log(`User ${userId} connected`);
+
+  ws.on("message", (data) => {
+    console.log(`Received message => ${data}`);
   });
   ws.send("pong!");
 });
