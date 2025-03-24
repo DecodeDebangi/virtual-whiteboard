02e45305a99ddd202500415e448e21f322fc93e5
Debangi <debangiofficial@gmail.com>
2025-03-24 22:58:09 +0530

    completed the signin endpoint and create-room in progress

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-// import dotenv from "dotenv";
 import cors from "cors";
 import { authMiddleware } from "./authMiddleware";
 import { JWT_SECRET } from "@repo/backend-common/config";
@@ -17,79 +16,111 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/signup", async (req: Request, res: Response) => {
-  const parsedData = CreateUserSchema.safeParse(req.body);
+app.post("/signup", async (req, res) => {
+  try {
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
+      res.status(400).json({ message: parsedData.error.errors });
+      return;
+    }
+    if (!parsedData.data.email || !parsedData.data.password) {
+      res.status(411).json({ message: "Invalid inputs" });
+      return;
+    }
+
+    const existingUser = await prismaClient.user.findUnique({
+      where: { email: parsedData.data?.email },
+    });
+    if (existingUser) {
+      res.status(409).json({ message: "User already exists with this email!" });
+      return;
+    }
+
+    const hashedPassword = await bcrypt.hash(parsedData.data?.password, 10);
+    const newUser = await prismaClient.user.create({
+      data: {
+        password: hashedPassword,
+        email: parsedData.data?.email,
+        username: parsedData.data?.username,
+      },
+    });
+
+    res.status(201).json({
+      message: "User created successfully",
+      user: { id: newUser.id, username: newUser.username },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+    console.error(error);
+  }
+});
+
+app.post("/signin", async (req, res) => {
+  try {
+    const parsedData = SigninSchema.safeParse(req.body);
+    if (!parsedData.success) {
+      res.status(400).json({ message: parsedData.error.errors });
+      return;
+    }
+
+    // const { username, password, email } = req.body;
+    if (!parsedData.data.email || !parsedData.data.password) {
+      res.status(411).json({ message: "Invalid inputs" });
+      return;
+    }
+
+    const existingUser = await prismaClient.user.findUnique({
+      where: { email: parsedData.data?.email },
+    });
+    if (
+      !existingUser ||
+      !(await bcrypt.compare(parsedData.data.password, existingUser.password))
+    ) {
+      res.status(401).json({ message: "Invalid username or password" });
+      return;
+    }
+
+    const token = jwt.sign({ userId: existingUser?.id }, JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ token: token, message: "Login successful" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+    console.error(error);
+  }
+});
+
+app.post("/create-room", authMiddleware, async (req, res) => {
+  const parsedData = CreateRoomSchema.safeParse(req.body);
   if (!parsedData.success) {
     res.status(400).json({ message: parsedData.error.errors });
     return;
   }
-  const { username, password, email } = req.body;
-  if (!username || !password || !email) {
-    res.status(411).json({ message: "Invalid inputs" });
+
+  const { roomId } = req.body;
+  if (
+    !roomId ||
+    roomId.length === 0 ||
+    roomId === "undefined" ||
+    roomId === "null" ||
+    roomId.length !== 6
+  ) {
+    res.status(411).json({ message: "Invalid room ID" });
     return;
   }
 
-  const existingUser = await prismaClient.user.findUnique({
-    where: { email: parsedData.data?.email },
-  });
-  if (existingUser) {
-    res.status(409).json({ message: "Username already exists" });
-    return;
-  }
-
-  const hashedPassword = await bcrypt.hash(parsedData.data?.password, 10);
-  const newUser = await prismaClient.user.create({
+  // Create a new room in the database
+  const newRoom = await prismaClient.room.create({
     data: {
-      username: parsedData.data?.username,
-      password: hashedPassword,
-      email: parsedData.data?.email,
+      slug: parsedData.data.name,
+      adminId: req.userId,
     },
   });
 
-  res.status(201).json({ message: "User created successfully" });
+  res.status(201).json({ message: "Room created successfully" });
+  return;
 });
-
-app.post("/signin", (req: Request, res: Response) => {
-  const validationResult = SigninSchema.safeParse(req.body);
-  if (!validationResult.success) {
-    res.status(400).json({ message: validationResult.error.errors });
-    return;
-  }
-
-  const { username, password } = req.body;
-  if (!username || !password) {
-    res.status(411).json({ message: "Invalid inputs" });
-    return;
-  }
-  // Check if the username exists in the database
-  // If user not found or password is incorrect, return an error
-
-  const token = jwt.sign({ username }, JWT_SECRET, {
-    expiresIn: "1h",
-  });
-
-  res.status(200).json({ token });
-});
-
-app.post(
-  "/create-room",
-  authMiddleware,
-  async (req: Request, res: Response) => {
-    const validationResult = CreateRoomSchema.safeParse(req.body);
-    if (!validationResult.success) {
-      res.status(400).json({ message: validationResult.error.errors });
-      return;
-    }
-    const { roomId } = req.body;
-    if (!roomId) {
-      res.status(411).json({ message: "Invalid room ID" });
-      return;
-    }
-    // Create a new room in the database
-    res.status(201).json({ message: "Room created successfully" });
-    return;
-  }
-);
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
 });
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,14 +1,15 @@
 import { z } from "zod";
 
 export const CreateUserSchema = z.object({
-  username: z.string().min(3).max(20),
+  username: z.string().min(3).max(20).optional().nullable(),
   password: z.string(),
   email: z.string().email(),
 });
 
 export const SigninSchema = z.object({
-  username: z.string().min(3).max(20),
+  username: z.string().min(3).max(20).optional().nullable(),
   password: z.string(),
+  email: z.string().email(),
 });
 
 export const CreateRoomSchema = z.object({
