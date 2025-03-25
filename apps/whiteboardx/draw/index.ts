c14d306f8b19cfc37178f5f0920ddb5705b647ae
Debangi <debangiofficial@gmail.com>
2025-03-26 03:37:33 +0530

    adding multiple rect by 1 user

--- apps/whiteboardx/draw/index.ts
+++ apps/whiteboardx/draw/index.ts
@@ -1,5 +1,31 @@
-const initDraw = (canvas: HTMLCanvasElement) => {
+import { clear } from "console";
+
+type Shape =
+  | {
+      type: "rect";
+      x: number;
+      y: number;
+      width: number;
+      height: number;
+    }
+  | {
+      type: "line";
+      x: number;
+      y: number;
+      x2: number;
+      y2: number;
+    }
+  | {
+      type: "circle";
+      x: number;
+      y: number;
+      radius: number;
+    };
+
+export const initDraw = (canvas: HTMLCanvasElement) => {
   const ctx = canvas.getContext("2d");
+
+  let existingShapes: Shape[] = [];
 
   if (!ctx) return;
   ctx.fillStyle = "rgba(0,0,0)";
@@ -18,6 +44,15 @@ const initDraw = (canvas: HTMLCanvasElement) => {
 
   canvas.addEventListener("mouseup", (e) => {
     clicked = false;
+    const width = e.clientX - startX;
+    const height = e.clientY - startY;
+    existingShapes.push({
+      type: "rect",
+      x: startX,
+      y: startY,
+      height: height,
+      width: width,
+    });
     console.log(e.clientX, e.clientY);
   });
 
@@ -26,13 +61,43 @@ const initDraw = (canvas: HTMLCanvasElement) => {
       const width = e.clientX - startX;
       const height = e.clientY - startY;
       console.log(e.clientX, e.clientY);
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = "rgba(0,0,0)";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      clearCanvas(existingShapes, canvas, ctx);
+
       ctx.strokeStyle = "rgba(255,255,255)";
       ctx.strokeRect(startX, startY, width, height);
     }
   });
 };
+
+function clearCanvas(
+  existingShapes: Shape[],
+  canvas: HTMLCanvasElement,
+  ctx: CanvasRenderingContext2D
+) {
+  if (!ctx) return;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "rgba(0,0,0)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  console.log(existingShapes);
+
+  existingShapes.map((shape) => {
+    if (shape.type === "rect") {
+      ctx.strokeStyle = "rgba(255,255,255)";
+      ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+    } else if (shape.type === "line") {
+      ctx.strokeStyle = "rgba(255,255,255)";
+      ctx.beginPath();
+      ctx.moveTo(shape.x, shape.y);
+      ctx.lineTo(shape.x2, shape.y2);
+      ctx.stroke();
+    } else if (shape.type === "circle") {
+      ctx.strokeStyle = "rgba(255,255,255)";
+      ctx.beginPath();
+      ctx.arc(shape.x, shape.y, shape.radius, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+  });
+}
 
 export default initDraw;
