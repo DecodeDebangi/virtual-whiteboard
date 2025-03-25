b3318d456fd2f937033c51231cc0e9c636a1ddd4
Debangi <debangiofficial@gmail.com>
2025-03-26 03:14:09 +0530

    restructured canvas rect

--- apps/whiteboardx/app/canvas/[roomId]/page.tsx
+++ apps/whiteboardx/app/canvas/[roomId]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import initDraw from "@/draw";
 import { useEffect, useRef } from "react";
-import { start } from "repl";
 
 const Canvas = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -9,42 +9,7 @@ const Canvas = () => {
   useEffect(() => {
     if (canvasRef.current) {
       const canvas = canvasRef.current;
-      const ctx = canvas.getContext("2d");
-
-      if (!ctx) return;
-
-      let clicked = false;
-      let startX = 0;
-      let startY = 0;
-
-      canvas.addEventListener("mousedown", (e) => {
-        clicked = true;
-        console.log(e.clientX, e.clientY);
-        startX = e.clientX;
-        startY = e.clientY;
-        // ctx.beginPath();
-        // ctx.moveTo(e.clientX, e.clientY);
-      });
-
-      canvas.addEventListener("mouseup", (e) => {
-        clicked = false;
-        console.log(e.clientX, e.clientY);
-        // ctx.beginPath();
-        // ctx.moveTo(e.clientX, e.clientY);
-      });
-
-      canvas.addEventListener("mousemove", (e) => {
-        if (clicked) {
-          const width = e.clientX - startX;
-          const height = e.clientY - startY;
-          console.log(e.clientX, e.clientY);
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.strokeRect(startX, startY, width, height);
-
-          //   ctx.beginPath();
-          //   ctx.moveTo(e.clientX, e.clientY);
-        }
-      });
+      initDraw(canvas);
     }
   }, [canvasRef]);
 
@@ -53,8 +18,8 @@ const Canvas = () => {
       <canvas
         ref={canvasRef}
         id='canvas'
-        width='1000'
-        height='1000'
+        width={2000}
+        height={2000}
         style={{ border: "1px solid black" }}></canvas>
     </div>
   );
---
+++ apps/whiteboardx/draw/index.ts
@@ -0,0 +1,38 @@
+const initDraw = (canvas: HTMLCanvasElement) => {
+  const ctx = canvas.getContext("2d");
+
+  if (!ctx) return;
+  ctx.fillStyle = "rgba(0,0,0)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  let clicked = false;
+  let startX = 0;
+  let startY = 0;
+
+  canvas.addEventListener("mousedown", (e) => {
+    clicked = true;
+    console.log(e.clientX, e.clientY);
+    startX = e.clientX;
+    startY = e.clientY;
+  });
+
+  canvas.addEventListener("mouseup", (e) => {
+    clicked = false;
+    console.log(e.clientX, e.clientY);
+  });
+
+  canvas.addEventListener("mousemove", (e) => {
+    if (clicked) {
+      const width = e.clientX - startX;
+      const height = e.clientY - startY;
+      console.log(e.clientX, e.clientY);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.fillStyle = "rgba(0,0,0)";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.strokeStyle = "rgba(255,255,255)";
+      ctx.strokeRect(startX, startY, width, height);
+    }
+  });
+};
+
+export default initDraw;
